14963bfa2446a7b70f63faa1ffd85f8797bfab9d
uwineza gloria <gloria7wineza@gmail.com>
2024-10-11 11:53:07 +0200

    adding school and user models

---
+++ models/school.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+const schoolSchema=mongoose.Schema({
+    SchoolName:{
+       type:String 
+    },
+    SchoolLogo:{
+type:Sting
+    },
+    SchoolAdrress:{
+Province:{
+    type:String
+},
+District:{
+type:String
+},
+Sector:{
+    type:String
+},
+Cell:{
+    type:String
+},
+Village:{
+    type:String
+}
+    },
+    SchoolBursar:{
+        FullName:{
+            type:String
+        },
+        Email:{
+            type:String
+        }
+    }
+})
+const schoolModel=mongoose.model("Schools",schoolSchema)
+export default schoolModel
